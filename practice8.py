6c5a239793cb20dbf4c352a38075f15586afe537
hutaw <yang84653@gmail.com>
2023-08-28 21:05:45 +0900

    Add test.py Add practice8.py

---
+++ practice8.py
@@ -0,0 +1,12 @@
+import math
+math.sqrt(16)
+print(dir(math))
+print(math.pi)
+print(math.log10(100))
+print(math.log10(1000))
+import calendar
+jan=calendar.month(2020,1)
+print(jan)
+calendar.isleap(2020)
+calendar.isleap(2019)
+
